b8562e564803ab74b217b5bd5262a65c040043c1
Akansha Tiwari <akanshat1999@gmail.com>
2026-09-02 01:05:58 +0530

    fix typo
    Signed-off-by: Akansha Tiwari <akanshat1999@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -31,8 +31,8 @@ export default function Home() {
           <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
             <div className="xl:max-w-wxl w-full max-w-xs lg:shrink-0">
               <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                Full-Stack Engineer. Production Obsessed. Always
-                And{' '}
+                Full-Stack Engineer. Production Obsessed. And Always
+                {' '}
                 <span className="bg-gradient-to-r from-aquamarine to-bright-pink bg-clip-text text-6xl text-transparent sm:text-7xl">
                   Shipping.
                 </span>
